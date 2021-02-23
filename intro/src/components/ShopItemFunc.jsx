74363dd49ed0112a5d61865bec42aaefca1aae78
kote-nikolaus <>
2021-02-24 00:45:27 +0300

    Initial commit

---
+++ intro/src/components/ShopItemFunc.jsx
@@ -0,0 +1,30 @@
+import './shop-item.css';
+import PropTypes from 'prop-types';
+
+export default function ShopItemFunc({ item }) {
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  )
+}
+
+ShopItemFunc.propTypes = {
+  item: PropTypes.shape({
+    brand: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+  })
+}
